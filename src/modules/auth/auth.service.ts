c6820169e8e83196f514d6d26f8bb29f0474ee65
max99xam <pepetrov123@list.ru>
2022-07-16 18:01:18 +0300

    removed HTTP-onlu from cookie response to client

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -66,7 +66,7 @@ class AuthService {
 	}
 
 	public createCookie(tokenData: TokenData): string {
-		return `Authorization=${tokenData.token}; HttpOnly; Max-Age=${tokenData.expiresIn};`;
+		return `Authorization=${tokenData.token}; Max-Age=${tokenData.expiresIn};`;
 	}
 }
 
