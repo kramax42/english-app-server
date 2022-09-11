3887fcebc41301760ce147e0581c6b2e64d0cebd
max <kramax42@yandex.by>
2022-09-11 12:23:13 +0300

    fixed IUserResponseDto interface

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -10,5 +10,8 @@ export interface User extends mongoose.Document {
 }
 
 export interface UserResponseDTO extends Omit<User, '_id' | 'password'> {
-	id: string;
+	id: string,
+	name: string;
+	email: string;
+	role: Role;
 }
